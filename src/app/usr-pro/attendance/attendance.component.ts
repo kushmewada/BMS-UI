ee7fc116132f0645d805d8192149242147325592
Chirag30111998 <chirag.t@peerbits.com>
2022-05-06 16:23:17 +0530

    add Clock in Calender view

--- src/app/usr-pro/attendance/attendance.component.ts
+++ src/app/usr-pro/attendance/attendance.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { map, share, Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-attendance',
@@ -6,33 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attendance.component.css'],
 })
 export class AttendanceComponent implements OnInit {
-  dates: any=[]
+  dates: any = [];
+
+  intervalId:any;
+  subscription!: Subscription;
+  rxTime = new Date();
+  
   constructor() {}
 
   ngOnInit(): void {
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = time;
+      });
+
     const getAllDaysInMonth = (month: any, year: any) =>
       Array.from(
-        { length: new Date(year, month, 0).getDate() }, // get next month, zeroth's (previous) day
-        (_, i) => new Date(year, month - 1, i + 1) // get current month (0 based index)
+        { length: new Date(year, month, 0).getDate() }, 
+        (_, i) => new Date(year, month - 1, i + 1),
       );
 
-      var month =
-      (new Date().getMonth() + 1).toString().slice(-2);
+    var month = (new Date().getMonth() + 1).toString().slice(-2);
 
-      var year =
-      new Date().getFullYear().toString();
-      // console.log(d)
-
+    var year = new Date().getFullYear().toString();
+    // console.log(d)
 
     const allDatesInOctober = getAllDaysInMonth(month, year);
 
-    this.dates = allDatesInOctober
+    this.dates = allDatesInOctober;
 
     console.log(
       allDatesInOctober.map((x) =>
         x.toLocaleDateString([], { month: 'short', day: 'numeric' })
       )
     );
-    console.log(this.dates,'my daya');
+    console.log(this.dates, 'my daya');
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
